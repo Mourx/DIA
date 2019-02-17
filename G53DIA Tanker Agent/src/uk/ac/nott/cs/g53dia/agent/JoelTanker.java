0c48891c145a529bf47d47a2a2d032e0d1829ee3
Joel <psyjc5@nottingham.ac.uk>
2019-02-17 13:23:01 +0000

    Added Scan for Wells and Stations
    Also added Task list population

--- G53DIA Tanker Agent/src/uk/ac/nott/cs/g53dia/agent/JoelTanker.java
+++ G53DIA Tanker Agent/src/uk/ac/nott/cs/g53dia/agent/JoelTanker.java
@@ -1,14 +1,20 @@
 package uk.ac.nott.cs.g53dia.agent;
 
+import java.util.ArrayList;
+
 import uk.ac.nott.cs.g53dia.library.Action;
 import uk.ac.nott.cs.g53dia.library.Cell;
 import uk.ac.nott.cs.g53dia.library.Station;
 import uk.ac.nott.cs.g53dia.library.Tanker;
 import uk.ac.nott.cs.g53dia.library.Task;
+import uk.ac.nott.cs.g53dia.library.Well;
 
 public class JoelTanker extends Tanker{
 
 	Task currentTask = null;
+	ArrayList<Station> NearbyStations;
+	ArrayList<Well> NearbyWells;
+	ArrayList<Task> AvailableTasks;
 	
 	@Override
 	public Action senseAndAct(Cell[][] view, boolean actionFailed, long timestep) {
@@ -16,8 +22,16 @@ public class JoelTanker extends Tanker{
 		for(int i = 0;i < this.VIEW_RANGE;i++) {
 			for(int j = 0;j < this.VIEW_RANGE;j++) {
 				if(view[j][i] instanceof Station) {
-					
+					NearbyStations.add((Station) view[j][i]);
+				}else if(view[j][i] instanceof Well) {
+					NearbyWells.add((Well) view[j][i]);
 				}
+			}
+		}
+		for(int i = 0;i<NearbyStations.size();i++) {
+			Task task = NearbyStations.get(i).getTask();
+			if(task != null) {
+				AvailableTasks.add(NearbyStations.get(i).getTask());
 			}
 		}
 		if(getCurrentCell(view) instanceof Station ) {
